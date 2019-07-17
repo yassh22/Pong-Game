66cedeafcdd7e76025800565daaf2af375e796ac
yassh22 <yassh22@yahoo.com>
2019-07-17 10:14:37 -0400

    feat:added svg canvas

--- src/partials/Game.js
+++ src/partials/Game.js
@@ -1,13 +1,32 @@
+import {SVG_NS} from '../settings.js'
+
 export default class Game {
   constructor(element, width, height) {
     this.element = element;
     this.width = width;
     this.height = height;
 
-		// Other code goes here...
+    // Other code goes here...
+    
+    this.gameElement = document.getElementById(this.element)
   }
 
   render() {
-		// More code goes here....
+
+    // Clear Board
+    this.gameElement.innerHTML = ''
+
+    // create SVG element for the Board
+    let svg = document.createElementNS(SVG_NS, 'svg')
+
+    svg.setAttributeNS(null, 'width', this.width)
+    svg.setAttributeNS(null, 'height', this.height)
+    svg.setAttributeNS(null, 'viewBox', `0 0 ${this.height} ${this.width}`)
+    this.gameElement.appendChild(svg)
+
+
+
+    
+
   }
 }
